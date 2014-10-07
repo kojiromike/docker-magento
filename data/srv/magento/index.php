e96e7ceb66362368dcb55d7a1198b8481e01b6bf
Michael A. Smith <msmith3@ebay.com>
2014-10-06 22:47:53 -0400

    Make Filler PHP More Informative.

--- data/srv/magento/index.php
+++ data/srv/magento/index.php
@@ -1,1 +1,13 @@
-<?php echo "it works";
+<!doctype html>
+<html>
+	<head>
+		<title>Set up</title>
+		<meta charset="utf-8">
+	</head>
+	<body>
+		<p>If you can read this, the web server is working.</p>
+		<?php
+			echo '<p>If you can read this, <a href="phpinfo.php">php</a> is working, too.</p>';
+			echo '<p>Now it&rsquo;s time to install Magento. Run the <code>init</code> container.</p>'; ?>
+	</body>
+</html>
